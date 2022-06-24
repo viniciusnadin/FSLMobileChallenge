110e24a52e48cb5dbde7ecc0d7e1d83b13dec5de
Thiago Galvani <tgalvani@fullstacklabs.co>
2022-06-24 14:34:27 -0300

    feat: creating the colors file

---
+++ toy-blocks-client-ios-uikit/toy-blocks-client-ios-uikit/Resources/Colors.swift
@@ -0,0 +1,28 @@
+//
+//  Colors.swift
+//  toy-blocks-client-ios-uikit
+//
+//  Created by Thiago Galvani on 23/06/22.
+//
+
+import UIKit
+
+extension UIColor {
+        
+    class func background() -> UIColor {
+        return UIColor(red: 0.953, green: 0.953, blue: 0.953, alpha: 1)
+    }
+    
+    class func lightGray() -> UIColor {
+        return UIColor(red: 0.149, green: 0.196, blue: 0.22, alpha: 1)
+    }
+    
+    class func green() -> UIColor {
+        return UIColor(red: 0.094, green: 0.8, blue: 0.334, alpha: 1)
+    }
+    
+    class func red() -> UIColor {
+        return UIColor(red: 0.922, green: 0.341, blue: 0.341, alpha: 1)
+    }
+    
+}
