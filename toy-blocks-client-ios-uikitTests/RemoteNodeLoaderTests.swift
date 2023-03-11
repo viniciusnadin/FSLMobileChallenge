a699ab468e887a11a03b3f673aac72a2819a8198
Vinicius Nadin <vininadin@gmail.com>
2023-03-11 19:19:58 -0300

    LoadNodeBlock command requests data from URL

--- toy-blocks-client-ios-uikitTests/RemoteNodeLoaderTests.swift
+++ toy-blocks-client-ios-uikitTests/RemoteNodeLoaderTests.swift
@@ -13,20 +13,45 @@ class RemoteNodeLoader {
     init(client: HTTPClient) {
         self.client = client
     }
+    
+    func loadNodeBlock(from url: URL) {
+        client.get(from: url)
+    }
 }
 
-protocol HTTPClient {}
+protocol HTTPClient {
+    func get(from url: URL)
+}
 
 final class RemoteNodeLoaderTests: XCTestCase {
     
     func test_init_doesNotMakeAPIRequest() {
+        let (_, client) = makeSUT()
+        
+        XCTAssertNil(client.receivedURL)
+    }
+    
+    func test_loadNodeBlock_requestsDataFromURL() {
+        let (sut, client) = makeSUT()
+        let url = URL(string: "http://any-url.com")!
+        
+        sut.loadNodeBlock(from: url)
+        
+        XCTAssertEqual(client.receivedURL, url)
+    }
+    
+    // MARK: - Helpers
+    private func makeSUT() -> (sut: RemoteNodeLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteNodeLoader(client: client)
-        
-        XCTAssertNil(client.receveidMessages)
+        return (sut, client)
     }
     
     private class HTTPClientSpy: HTTPClient {
-        var receveidMessages: Any? = nil
+        var receivedURL: URL?
+        
+        func get(from url: URL) {
+            receivedURL = url
+        }
     }
 }
